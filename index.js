c94d5b28deceefc824b7475a64d950da171e9245
roman-the-earth <romantheearth@gmail.com>
2023-02-17 01:46:44 +1300

    update index.js for html

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = express()
 app.all('/', (req, res) => {
     // console.log("Just got a request!")
     // res.send('Yo! Whats up')
-    res.redirect('/index.html')
+    res.redirect('index.html')
 })
 app.listen(process.env.PORT || 3000)
