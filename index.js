3cf167c34661547ea368450e272df295b1b66374
roman-the-earth <romantheearth@gmail.com>
2023-02-17 01:42:53 +1300

    update index.js for html

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo! Whats up')
+    // console.log("Just got a request!")
+    // res.send('Yo! Whats up')
+    res.redirect('/index.html')
 })
 app.listen(process.env.PORT || 3000)
