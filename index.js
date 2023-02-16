4c3721fa1bd64896d3093d0c7d678ac88a05031b
roman-the-earth <romantheearth@gmail.com>
2023-02-17 01:19:12 +1300

    testing changes

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!')
+    res.send('Yo! Whats up')
 })
 app.listen(process.env.PORT || 3000)
