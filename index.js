5494050237e7173b632d90b59f999c589c8a1968
roman-the-earth <romantheearth@gmail.com>
2023-02-18 18:11:09 +1300

    adding convincers WIP

--- index.js
+++ index.js
@@ -1,8 +1,45 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    // console.log("Just got a request!")
-    // res.send('Yo! Whats up')
-    res.redirect('index.html')
-})
-app.listen(process.env.PORT || 3000)
+/**
+ * Required External Modules
+ */
+ const express = require('express');
+ const path = require('path');
+
+/**
+ * App Variables
+ */
+const app = express();
+const port = process.env.PORT || "8000";
+
+/**
+ *  App Configuration
+ */
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
+ 
+/**
+ * Routes Definitions
+ */
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" });
+});
+
+app.get("/gigs", (req, res) => {
+  res.render("gigs", { title: "Gigs" });
+});
+
+app.get("/booking", (req, res) => {
+  res.render("booking", { title: "Bookings" });
+});
+
+app.get("/gallery", (req, res) => {
+  res.render("gallery", { title: "Gallery" });
+});
+
+/**
+ * Server Activation
+ */
+ app.listen(port, () => {
+    console.log(`Listening for convincers requests on http://localhost:${port}`) ;
+  });
